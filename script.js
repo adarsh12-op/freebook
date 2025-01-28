bd38d025243a2e7ff97a53640751ec6c4ddf3a48
adarsh12-op <adarshrajsingh121@gmail.com>
2025-01-29 00:22:44 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+let keyFeatureEnabled = true;
+let keyGenerated = false;
+const videoPlayer = document.getElementById('videoPlayer');
+const videoSource = document.getElementById('videoSource');
+
+const videoLink = "https://d1qcficr3lu37x.cloudfront.net/file_library/videos/channel_vod_non_drm_hls/4278007/173644262877098296296383/index_1.m3u8";
